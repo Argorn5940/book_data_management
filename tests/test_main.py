cf8f995a7792aaf62e97e17e535235de21a09caf
Argorn5940 <sh.05940@gmail.com>
2024-10-31 21:09:28 +0900

    Update test code in tests/test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,5 @@
-
-import  mock 
-from unittest import mock
-import main
 import unittest
 
 class TestMain(unittest.TestCase):  # TestCaseを継承
     def test_example(self):  # テストメソッドはtest_で始まる必要がある
-        # ここにテストコードを追加
         self.assertEqual(1, 1)  
-        
-    # テスト用のデータを準備
-main.bookname_entry = mock.Mock()
-main.authorname_entry = mock.Mock()
-main.price_entry = mock.Mock()
